550485d0119bda44fe193a035a234559221963c0
Thomas Vincent <thomasvincent@gmail.com>
2026-04-08 22:59:40 -0700

    refactor(php74): use null coalescing (??) and ??= operators
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- Text/Exception/Pear.php
+++ Text/Exception/Pear.php
@@ -54,11 +54,11 @@ class Horde_Exception_Pear extends Horde_Exception {
 
 			foreach ($backtrace as $frame) {
 				$pear_error .=
-					  (isset($frame['class']) ? $frame['class'] : '')
-					. (isset($frame['type']) ? $frame['type'] : '')
-					. (isset($frame['function']) ? $frame['function'] : 'unkown') . ' '
-					. (isset($frame['file']) ? $frame['file'] : 'unkown') . ':'
-					. (isset($frame['line']) ? $frame['line'] : 'unkown') . "\n";
+					  ($frame['class'] ?? '')
+					. ($frame['type'] ?? '')
+					. ($frame['function'] ?? 'unkown') . ' '
+					. ($frame['file'] ?? 'unkown') . ':'
+					. ($frame['line'] ?? 'unkown') . "\n";
 			}
 		}
 		$userinfo = $error->getUserInfo();
